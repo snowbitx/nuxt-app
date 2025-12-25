a640f51253ae5249c2a0334f5770aa5d77965254
snowbitx <WangXiaoYu_1995@outlook.com>
2025-12-25 23:36:15 +0800

    init nuxt app

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from '@antfu/eslint-config'
+export default withNuxt(
+  antfu({
+    // Type of the project. 'lib' for libraries, the default is 'app'
+    type: 'lib',
+
+    // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+    // The `ignores` option in the option (first argument) is specifically treated to always be global ignores
+    // And will **extend** the config's default ignores, not override them
+    // You can also pass a function to modify the default ignores
+    ignores: [
+      '**/fixtures',
+      // ...globs
+    ],
+
+    // Parse the `.gitignore` file to get the ignores, on by default
+    gitignore: true,
+
+    // Enable stylistic formatting rules
+    stylistic: true,
+
+    // Or customize the stylistic rules
+    stylistic: {
+      indent: 2, // 4, or 'tab'
+      quotes: 'single', // or 'double'
+    },
+
+    // TypeScript and Vue are autodetected, you can also explicitly enable them:
+    typescript: true,
+    vue: true,
+
+    // Disable jsonc and yaml support
+    jsonc: false,
+    yaml: false,
+    // antfu 的 ignore（Nuxt 会 merge）
+    ignores: [
+      '**/fixtures',
+      'dist',
+      '.output',
+    ],
+  }),
+  // Your custom configs here
+)
