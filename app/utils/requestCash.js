54881c93a1c98c53bb39e274e3eaf5473a2954ff
snowbitx <WangXiaoYu_1995@outlook.com>
2026-01-10 15:54:35 +0800

    feat:add request cash

---
+++ app/utils/requestCash.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @param {*} fn 返回promise的函数 
+ * @description  限制重复请求
+ */
+export function createRequestCash(fn) {
+  const map = {}
+  return (...args) => {
+    return (map[JSON.stringify(args)] ??= fn(...args).finally(() => {
+      delete map[JSON.stringify(args)]
+    }))
+  }
+}
+
+const loadData = createRequestCash(function () {
+  return new Promise((resolve, reject) => {
+    console.log('发请求了');
+    setTimeout(() => {
+      resolve({ id: 1 })
+    }, 1000);
+  });
+})
+
+loadData().then((res) => {
+  console.log('res :>> ', res);
+})
+loadData().then((res) => {
+  console.log('res :>> ', res);
+})
